2d99fdad3f73b31abe86253c75510dc1e636a48c
Cristiana Man <man.cristiana1@gmail.com>
2017-12-01 20:51:38 +0100

    Chaged structure of returned token value inside Spotify Controller

--- src/SoundsUp.WebHost/Controllers/SpotifyController.cs
+++ src/SoundsUp.WebHost/Controllers/SpotifyController.cs
@@ -34,7 +34,7 @@ namespace SoundsUp.WebHost.Controllers
 
             Token token = credentials.DoAuth();
 
-            return Ok(new {Token = token});
+            return Ok(token);
         }
     }
 }
