88672552240339cf5e873a3951f572f4d4bd3d36
EliiseS <selingeliise@gmail.com>
2017-12-07 22:06:40 +0100

    Add validation for Register

--- src/SoundsUp.Domain/Entities/RegisterViewModel.cs
+++ src/SoundsUp.Domain/Entities/RegisterViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace SoundsUp.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoundsUp.Domain.Entities
 {
     public class RegisterViewModel
     {
+        [Required]
         public string Password { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string DisplayName { get; set; }
         public string Description { get; set; }
         public string Avatar { get; set; }
