e6ed061408ca828fb24b1a307cba78e549b1db12
EliiseS <selingeliise@gmail.com>
2017-12-10 19:53:38 +0100

    Add conversation

---
+++ src/SoundsUp.Domain/Entities/ConversationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SoundsUp.Domain.Entities
+{
+    public class ConversationViewModel : Account
+    {
+        public DateTime TimeStamp { get; set; }
+    }
+}
--- src/SoundsUp.Domain/Entities/ParticipantsViewModel.cs
+++ src/SoundsUp.Domain/Entities/ParticipantsViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SoundsUp.Domain.Entities
+﻿namespace SoundsUp.Domain.Entities
 {
     public class ParticipantsViewModel
     {
