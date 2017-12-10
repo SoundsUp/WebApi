aed0313a8d232542e01f971b7f44eef79d026078
EliiseS <selingeliise@gmail.com>
2017-12-10 11:20:29 +0100

    Add minium length requirement for password

--- src/SoundsUp.Domain/Entities/RegisterViewModel.cs
+++ src/SoundsUp.Domain/Entities/RegisterViewModel.cs
@@ -4,6 +4,7 @@ namespace SoundsUp.Domain.Entities
     public class RegisterViewModel
     {
         [Required]
+        [MinLength(12, ErrorMessage = "Must be between 12 and 255 characters")]
         public string Password { get; set; }
         [EmailAddress]
         public string Email { get; set; }
