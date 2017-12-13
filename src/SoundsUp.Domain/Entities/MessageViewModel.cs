e4e7b54b362782abf5ecaee830593264283eb9e0
Cristiana Man <man.cristiana1@gmail.com>
2017-12-13 18:54:58 +0100

    Finished Message Post endpoint

--- src/SoundsUp.Domain/Entities/MessageViewModel.cs
+++ src/SoundsUp.Domain/Entities/MessageViewModel.cs
@@ -3,11 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SoundsUp.Domain.Entities
 {
-    public class MessageViewModel
+    public class MessageViewModel : MessageContentViewModel
     {
         public int UserFrom { get; set; }
         public int UserTo { get; set; }
-        [Required]
-        public string MsgContent { get; set; }
     }
 }
